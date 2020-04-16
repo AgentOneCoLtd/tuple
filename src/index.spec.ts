d730dc47b2552b7fef9ad47f62483db228819220
Krittanan Pingclasai <kp@kpping.me>
2020-04-16 11:17:32 +0000

    lint happy

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,7 +3,10 @@ import { IKeyValTuple, toObject } from './index';
 it('should get { a: "a", b: "b" }', () => {
     type fooType = { a: string; b: string };
 
-    const tupleList: IKeyValTuple<fooType>[] = [['a', 'a'], ['b', 'b']];
+    const tupleList: IKeyValTuple<fooType>[] = [
+        ['a', 'a'],
+        ['b', 'b'],
+    ];
 
     expect(toObject(tupleList)).toEqual({ a: 'a', b: 'b' });
 });
